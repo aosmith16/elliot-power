f3e193ce0fafe8acb7115ffcf8eb754e9a21e442
Ariel Muldoon <ariel.muldoon@oregonstate.edu>
2020-11-05 11:22:13 -0800

    Make facet lollipops

--- 2020-11_elliott_power_analysis.R
+++ 2020-11_elliott_power_analysis.R
@@ -153,24 +153,68 @@ respdat_years = filter(respdat, Year %in% c(20, 50, 100) )
 #                 '50' = "Year 50", 
 #                 '100' = "Year 100")
 
+# May want to set limits of boxplots all the same
+# Test with OCWA
+ocwalims = range(pull(respdat_years, OCWA10))
+
+
 # Test boxplot for one response for one year
-ggplot(data = filter(respdat_years, Year == 20), aes(x = Treatment, y = CARBac) ) +
+ggplot(data = filter(respdat_years, Year == 20), aes(x = Treatment, y = OCWA10) ) +
     geom_boxplot(aes(color = Treatment)) +
     geom_point(aes(color = Treatment), size = 2) +
     scale_color_manual(values = colors, guide = "none") +
-    scale_fill_manual(values = colors, guide = "none")  +
+    # scale_fill_manual(values = colors, guide = "none")  +
+    scale_y_continuous(limits = ocwalims) +
     stat_summary(fun = mean, geom = "point", shape = 18,
                  size = 3.5, color = "grey24") +
     # facet_wrap(~Year, scales = "free_y", labeller = labeller(Year = fac_labels)) +
     theme_bw(base_size = 12) +
-    labs(y = respnames["CARBac"],
+    labs(y = "Birds per 10 acres",
          x = NULL) +
     theme(panel.grid.major.x = element_blank(),
           panel.grid.minor.y = element_blank(),
           axis.text = element_text(color = "black") )
 
 # Test saving for save and general look
-# ggsave("test.png", path = here::here("plots"),
+# ggsave("testbox1.png", path = here::here("plots"),
+#        width = 4, height = 3, dpi = 300)
+
+ggplot(data = filter(respdat_years, Year == 50), aes(x = Treatment, y = OCWA10) ) +
+    geom_boxplot(aes(color = Treatment)) +
+    geom_point(aes(color = Treatment), size = 2) +
+    scale_color_manual(values = colors, guide = "none") +
+    # scale_fill_manual(values = colors, guide = "none")  +
+    scale_y_continuous(limits = ocwalims) +
+    stat_summary(fun = mean, geom = "point", shape = 18,
+                 size = 3.5, color = "grey24") +
+    # facet_wrap(~Year, scales = "free_y", labeller = labeller(Year = fac_labels)) +
+    theme_bw(base_size = 12) +
+    labs(y = "Birds per 10 acres",
+         x = NULL) +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          axis.text = element_text(color = "black") )
+
+# ggsave("testbox2.png", path = here::here("plots"),
+#        width = 4, height = 3, dpi = 300)
+
+ggplot(data = filter(respdat_years, Year == 100), aes(x = Treatment, y = OCWA10) ) +
+    geom_boxplot(aes(color = Treatment)) +
+    geom_point(aes(color = Treatment), size = 2) +
+    scale_color_manual(values = colors, guide = "none") +
+    # scale_fill_manual(values = colors, guide = "none")  +
+    scale_y_continuous(limits = ocwalims) +
+    stat_summary(fun = mean, geom = "point", shape = 18,
+                 size = 3.5, color = "grey24") +
+    # facet_wrap(~Year, scales = "free_y", labeller = labeller(Year = fac_labels)) +
+    theme_bw(base_size = 12) +
+    labs(y = "Birds per 10 acres",
+         x = NULL) +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          axis.text = element_text(color = "black") )
+
+# ggsave("testbox3.png", path = here::here("plots"),
 #        width = 4, height = 3, dpi = 300)
 
 # Pad year with 0 for naming saved plots
@@ -194,7 +238,6 @@ boxplot_fun = function(data, yvar) {
         geom_boxplot(aes(color = Treatment)) +
         geom_point(aes(color = Treatment), size = 2) +
         scale_color_manual(values = colors, guide = "none") +
-        scale_fill_manual(values = colors, guide = "none")  +
         stat_summary(fun = mean, geom = "point", shape = 18,
                      size = 3.5, color = "black") +
         theme_bw(base_size = 12) +
@@ -370,13 +413,19 @@ power = power %>%
 # write.csv(power,
 #           file = here::here("results", "2020-11_ESRF_estimated_power.csv"),
 #           row.names = FALSE)
+# Read back in if need again
+# power = read.csv(here::here("results", "2020-11_ESRF_estimated_power.csv"))
 
 
 # Graph power with lollipop charts ----
 
-# This will involve splitting the power dataset for looping
-power_split = split(power, list(power$Response, power$Year) )
+# 2020-11-05 Scott requested we get rid of samples sizes <6
+power = filter(power, !n %in% c(4, 5) )
 
+# This will involve splitting the power dataset for looping
+# Add leading 0 before split by year
+power$Year = sprintf("%03d", power$Year)
+power_split = split(power, list(power$Response, power$Year) )
 
 # Test plot
 # Basing theme off example in ggalt (code on github)
@@ -401,6 +450,7 @@ ggplot(data = power_split[[1]], aes(x = Power, y = stringr::str_wrap(n, width = 
 # Test sizes when saving
 # ggsave("test_lolli.png", path = here::here("plots"),
 #        width = 3, height = 3, dpi = 300)
+
 
 # Function for plotting
 lollipop_fun = function(data) {
@@ -437,3 +487,136 @@ iwalk(all_lolli, ~ggsave(filename = paste0("2020-11_ESRF_lollipop_",
                          dpi = 300))
 
 
+# 2020-11-05 May need to keep years together in one plot
+power_split2 = split(power, power$Response)
+
+# Remember year is a character with leading 0 but will now
+    # want to remove those if keep a legend
+
+# Chose colors by getting 7 and taking 3, 5, 7 
+colorspace::specplot(colorspace::sequential_hcl(n = 7,
+                           palette = "Blues", rev = TRUE))
+dput(colorspace::sequential_hcl(n = 7,
+                                palette = "Blues", rev = TRUE))
+# col2rgb(c("#ACCCE4", "#5087C1", "#273871"))
+# [,1] [,2] [,3]
+# red    172   80   39
+# green  204  135   56
+# blue   228  193  113
+
+# Make labels for facets
+fac_labels = c('020' = "20 years",
+                '050' = "50 years",
+                '100' = "100 years")
+
+# Use facet labels on right instead of legend
+ggplot(data = power_split2[[1]], aes(x = Power, y = stringr::str_wrap(n, width = 8) )  ) +
+    geom_segment(aes(x = 0, xend = Power, y = stringr::str_wrap(n, width = 8), yend = stringr::str_wrap(n, width = 8))) +
+    geom_point(aes(color = factor(as.numeric(Year))) , size = 2) + 
+    facet_wrap(~Year, ncol = 1, strip.position = "right", 
+               labeller = labeller(Year = fac_labels) ) +
+    scale_x_continuous(expand = expansion(add = c(0, .02) ),                                                                                                       
+                       breaks=seq(0, 1, by = 0.2), limits = c(0, 1) ) +
+    labs(y = NULL) +
+    scale_color_manual(values = c("#ACCCE4", "#5087C1", "#273871"),
+                       guide = "none") +
+    theme_minimal(base_size = 12) +
+    theme(panel.grid.major.y = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.ticks.length.x = unit(0, "pt"),
+          axis.text.y = element_text(color = "black", margin = margin(l = 0, r = 0) ),
+          axis.text.x = element_text(color = "black"),
+          panel.spacing = unit(0, "lines"),
+          #plot.margin = margin(0, 0, 0, 0)
+    )
+
+# Test sizes when saving
+# ggsave("test_lolli.png", path = here::here("plots"),
+#        width = 3, height = 4, dpi = 300)
+
+# Function for plotting
+lollipop_fun2 = function(data) {
+    ggplot(data = data, aes(x = Power, y = stringr::str_wrap(n, width = 8) ) ) +
+        geom_segment(aes(x = 0, xend = Power, y = stringr::str_wrap(n, width = 8), yend = stringr::str_wrap(n, width = 8))) +
+        geom_point(aes(color = factor(as.numeric(Year))) , size = 2) + 
+        facet_wrap(~Year, ncol = 1, strip.position = "right", 
+                   labeller = labeller(Year = c('020' = "20 years",
+                                                '050' = "50 years",
+                                                '100' = "100 years") ) ) +
+        scale_x_continuous(expand = expansion(add = c(0, .02) ),                                                                                                       
+                           breaks=seq(0, 1, by = 0.2), limits = c(0, 1) ) +
+        labs(y = NULL) +
+        scale_color_manual(values = c("#ACCCE4", "#5087C1", "#273871"),
+                           guide = "none") +
+        theme_minimal(base_size = 12) +
+        theme(panel.grid.major.y = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.ticks.length.x = unit(0, "pt"),
+              axis.text.y = element_text(color = "black", margin = margin(l = 0, r = 0) ),
+              axis.text.x = element_text(color = "black"),
+              panel.spacing = unit(0, "lines"),
+              #plot.margin = margin(0, 0, 0, 0) 
+              )
+}
+
+    
+# Run through all datasets and plot
+all_lolli2 = map(power_split2, lollipop_fun2)
+
+# Save all plots with names based on response and year
+iwalk(all_lolli2, ~ggsave(filename = paste0("2020-11_ESRF_lollipop_", 
+                                           .y, "_", "allyears", ".png"), 
+                         path = here::here("plots"),
+                         plot = .x, 
+                         height = 4, 
+                         width = 3,
+                         units = "in",
+                         dpi = 300))
+
+# Using dodging instead of facets (not needed)
+# Turns out dodging doesn't work correctly for segments
+# power$nstring = factor(stringr::str_wrap(power$n, width = 8))
+# power_split2 = split(power, power$Response)
+# 
+# ggplot(data = power_split2[[1]], aes(x = Power, y = nstring)  ) +
+#     geom_blank() + # to get discrete scale to work
+#     geom_segment(aes(x = 0, xend = Power, 
+#                      group = Year,
+#                      y = case_when(Year == "050" ~ as.numeric(nstring),
+#                                    Year == "020" ~ as.numeric(nstring) + .3,
+#                                    Year == "100" ~ as.numeric(nstring) - .3), 
+#                      yend = case_when(Year == "050" ~ as.numeric(nstring),
+#                                       Year == "020" ~ as.numeric(nstring) + .3,
+#                                       Year == "100" ~ as.numeric(nstring) - .3) ) ) +
+#     geom_point(aes(color = factor(as.numeric(Year), levels = c(100, 50, 20))) , size = 2,
+#                position = position_dodge(width = .9) ) + 
+#     scale_x_continuous(expand = expansion(add = c(0, .02) ),                                                                                                       
+#                        breaks=seq(0, 1, by = 0.2), limits = c(0, 1) ) +
+#     labs(y = NULL) +
+#     scale_color_manual(values = rev(c("#7FABD3", "#3573B9", "#273871")),
+#                        name = "Year", guide = guide_legend(direction = "horizontal",
+#                                                            reverse = TRUE)) +
+#     theme_minimal(base_size = 12) +
+#     theme(panel.grid.major.y = element_blank(),
+#           panel.grid.minor = element_blank(),
+#           axis.ticks.length.x = unit(0, "pt"),
+#           axis.text.y = element_text(color = "black", margin = margin(l = 0, r = 0) ),
+#           axis.text.x = element_text(color = "black"),
+#           legend.position = "top",
+#           legend.justification = "right",
+#           legend.margin = margin(0, 0, 0, 0),
+#           panel.spacing = unit(0, "lines"),
+#           legend.box.margin = margin(-10,-10,-10, r = 0),
+#           legend.spacing.x = unit(0, unit = "pt"),
+#           legend.title = element_text(size = 11)
+#           
+#           # axis.line.y = element_line(size = 0.15)
+#           # axis.title.y = element_markdown(vjust = 10, 
+#           #                                 margin = unit(c(r = -10, 0, 0, 0), "pt")
+#           #                                 ) 
+#     )
+
+
+# Test sizes when saving
+# ggsave("test_lolli2.png", path = here::here("plots"),
+#        width = 3, height = 4, dpi = 300)
